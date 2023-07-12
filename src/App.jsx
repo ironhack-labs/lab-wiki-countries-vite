d91c40dd14d798e857c7098c6647f7f4e8e77f6a
alexanderley <alexander.ley.inbox@gmail.com>
2023-07-12 11:12:54 +0200

    Lab done some error occur

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,80 @@
-function CountryDetails() {}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
-export default CountryDetails;
+export default function CountryDetailsPage(props) {
+  const [data, setData] = useState([]);
+  const [borders, setBorders] = useState([]);
+  const { countryId } = useParams();
+  //   console.log("countryId:", countryId);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://ih-countries-api.herokuapp.com/countries/${countryId}`
+        );
+        setData(response.data);
+        setBorders(response.data.borders);
+        console.log("data:", data);
+        console.log("Borders:", data.borders);
+        console.log("borders", borders);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [countryId]);
+
+  return (
+    <div className="container">
+      <h1>{data.common.name}</h1>
+      <img
+        src={`https://flagpedia.net/data/flags/icon/72x54/${data.alpha2Code.toLowerCase()}.png`}
+        alt=""
+        style={{ width: "100px", marginRight: "10px" }}
+      />
+      {/* {data.name.common && data.alpha2Code && (
+        <div>
+          <h1>{data.name.common}</h1>
+          <img
+            src={`https://flagpedia.net/data/flags/icon/72x54/${data.alpha2Code.toLowerCase()}.png`}
+            alt=""
+            style={{ width: "100px", marginRight: "10px" }}
+          />
+        </div>
+      )} */}
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: "30%" }}>Capital</td>
+            <td>{data.capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {`${data.area}`} km<sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+
+            <td>
+              <ul>
+                {borders &&
+                  Array.isArray(borders) &&
+                  borders.map((border, index) => (
+                    <li key={index}>
+                      <Link to={`/${border}`}>{border}</Link>
+                    </li>
+                  ))}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
