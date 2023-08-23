8ec0e6fddcfba20c65ac06b35fe5bd3f51c8bf4e
Joan Benaiges <jobeto87@gmail.com>
2023-08-23 18:59:05 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
 
+import {Routes, Route} from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import Homepage from "./pages/HomePage"
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Homepage />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
+
+      </Routes>
+
     </div>
   );
 }
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,36 @@
-function CountryDetails() {}
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
+
+
+function CountryDetails() {
+
+    const {countryId} = useParams()
+
+    const [countryData, setCountryData] = useState("") 
+ 
+    useEffect(() => {
+    
+        axios
+            .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`) 
+            .then(response => {
+                setCountryData(response.data)
+
+            })
+      
+            .catch(err => console.log(err))
+
+        }, [countryId])
+
+    return (
+  
+    <div>
+        <h1>{countryData.name ? countryData.name.common : "Loading..."}</h1>
+        <p>Area: {countryData.area} km2</p>
+        <p>Capital: {countryData.capital}</p>
+    </div>
+    );
+}
 
 export default CountryDetails;
