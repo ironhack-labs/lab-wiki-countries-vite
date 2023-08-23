cfa86f4492e66d79972e4f7e24a4623070c5ace3
Claudia Alvarez Posada <daibhaith@hotmail.com>
2023-08-23 20:25:50 +0200

    Done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,45 @@
-function HomePage() {}
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default HomePage;
+function HomePage() {
+
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+
+        axios.get(`https://ih-countries-api.herokuapp.com/countries`).then((countries) => {
+            // console.log(countries.data)
+            setCountries(countries.data);
+        });
+
+    }, []);
+
+    return (
+
+        <div class="list-group">
+
+            <p style={{ fontSize: "20px", fontWeight: "bold" }}>WikiCountries: Your Guide to the World</p>
+            {
+                countries.map((country) => {
+
+                    return (
+                        <div style={{ maxHeight: "70px" }}>
+                            <ul>
+                                <img src={` https://flagpedia.net/data/flags/icon/72x54/${(country.alpha2Code).toLowerCase()}.png`} class="img" />
+                                <Link to={`/${country.alpha3Code}`}>{country.name.common}</Link>
+                            </ul>
+                        </div>
+                    )
+                })
+            }
+
+        </div >
+    )
+}
+
+export default HomePage
+
+
+
