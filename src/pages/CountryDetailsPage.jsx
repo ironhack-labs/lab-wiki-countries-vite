878b42e88565746c26b05fb2ea0daf7146928ef5
0000nebu <neboa.fresco@gmail.com>
2023-10-15 19:06:34 +0200

    Done but ugly

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,31 @@
-function CountryDetails() {}
+import React from 'react'
+import {useState, useEffect } from 'react';
+import {useParams} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default CountryDetails;
+export default function CountryDetailsPage() {
+  const [data, setData] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+      fetch(`https://ih-countries-api.herokuapp.com/countries/${params.alpha3Code}`)
+      .then(res => res.json()) 
+      .then(data => {
+          setData(data)  
+      })
+  }, []);
+
+  return <>
+  <h3>{data?.name.common}</h3>
+  <p>capital {data?.capital}</p>
+  <p>area {data?.area}m2</p>
+  <div>
+  {data?.borders.map((border) => (
+     <div key={border}>
+     <Link to={`/countries/${border}`}>{border}</Link> )
+     </div>
+    ))}
+  </div>
+  </>
+}
+
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,30 @@
-function HomePage() {}
+import React, { useEffect, useState } from 'react';
+import "./HomePage.css";
+import { Link } from 'react-router-dom';
 
-export default HomePage;
+export default function HomePage() {
+    
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch("https://ih-countries-api.herokuapp.com/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+        });
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>WikiCountries: Your Guide to the World</h1>
+            <div className="list-group">
+                {data.map((country) => (
+                    <div key={country.alpha3Code}>
+                       <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha3Code.toLowerCase()}.png`}  alt={country.name.common} />
+                        <Link to={`/countries/${country.alpha3Code}`}>{country.name.common}</Link> 
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
