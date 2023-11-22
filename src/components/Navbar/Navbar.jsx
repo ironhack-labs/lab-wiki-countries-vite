cd343e937978f8208aa0e980561b8b7f4cf1403b
Marina Viejo <100945399+marinavp8@users.noreply.github.com>
2023-11-22 21:06:09 +0100

    done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom"
+import "./Navbar.css"
+
+
+function Navbar() {
+
+    return (
+        <nav class="navbar navbar-dark bg-primary mb-3">
+            <div class="container">
+                <a class="navbar-brand" href="/">WikiCountries</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
