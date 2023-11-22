059e3849cbe28566827caab4b2125f425e513c57
Elena Ceinos <elenaceinos@hotmail.com>
2023-11-22 21:38:10 +0100

    scrolling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,12 +20,19 @@ function HomePage() {
     return (
         <>
             <h1 className="titlePage">WikiCountries: Your Guide to the World</h1>
-            <ul className="list-group">
-                {data ?
-                    data.map(e => (<Link className="list-group-item list-group-item-action" key={e._id} to={`/${e.alpha3Code}`}><span className="countryName">{e.name.common} </span>  <img src={`https://flagpedia.net/data/flags/icon/72x54/${e.alpha2Code.toLowerCase()}.png`} alt="flag"></img></Link>))
-                    : 'Loading...'
-                }
-            </ul >
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div className="list-container" style={{ maxHeight: '500px', maxWidth: '600px', overflowY: 'auto', border: '2px solid black' }}>
+                    {data ?
+                        data.sort((a, b) => a.name.common.localeCompare(b.name.common)).map(e => (
+                            <Link className="list-group-item list-group-item-action" key={e._id} to={`/${e.alpha3Code}`}>
+                                <span className="countryName">{e.name.common} </span>
+                                <img src={`https://flagpedia.net/data/flags/icon/72x54/${e.alpha2Code.toLowerCase()}.png`} alt="flag"></img>
+                            </Link>
+                        ))
+                        : 'Loading...'
+                    }
+                </div>
+            </div >
         </>
     )
 
