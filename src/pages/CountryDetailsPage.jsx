55644d37c2faf03f0db54d0bfa27f8cdfed16352
Fabio García <fabiogcaam@gmail.com>
2023-11-22 19:47:18 +0100

    done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,60 @@
-function CountryDetails() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+function CountryDetails() {
+
+    const { countryId } = useParams()
+    const [country, setCountry] = useState()
+
+
+    useEffect(() => {
+        axios
+            .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+            .then(response => setCountry(response.data))
+            .catch(err => console.log(err))
+    }, [countryId])
+
+    console.log("---------------", country)
+
+    return (
+        country
+            ?
+            <div className="CountryDetails">
+                < h1 > Country Details</h1 >
+                <div>
+                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={country.name.common} />
+                    <p>{country.name.common}</p>
+                </div>
+                <div>
+                    <div>
+                        <p>Capital:    {country.capital[0]}</p>
+                    </div>
+                    <div>
+                        <p>Area:        {country.area} km</p>
+                    </div>
+
+                    <div>
+                        <p>Borders:
+
+                            {country.borders.map(elm => {
+                                return < li ><Link to={`/${elm}`}>{elm}</Link></li>
+                            })}
+
+                        </p>
+
+                    </div>
+                </div >
+            </div >
+
+            :
+            <div>
+                < h1 > Country Details</h1 >
+                <h1>Loading...</h1>
+            </div>
+
+
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,43 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('https://ih-countries-api.herokuapp.com/countries')
+            .then(response => setCountries(response.data))
+            .catch(err => console.log(err))
+
+    }, [])
+
+    console.log(countries)
+    return (
+        <div className="HomePage container">
+            {<div className="row">
+                <h1>WikiCountries: Your Guide to the World</h1>
+                {
+                    countries.map(elm => {
+                        return <div key={elm._id} className="col-12">
+                            <div className="row">
+                                <p>
+
+                                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${elm.alpha2Code.toLowerCase()}.png`} alt={elm.name.common} />
+
+                                    <Link to={`/${elm.alpha3Code}`} >{elm.name.common}</Link>
+                                </p>
+                            </div>
+                            <hr />
+                        </div>
+                    })
+                }
+            </div>
+            }
+        </div>
+    )
+}
 
 export default HomePage;
