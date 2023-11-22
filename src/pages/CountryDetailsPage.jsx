aacb12842b7f982be7308d7be6c36eab01b6859e
Jormag <jorgemamblonagomez@gmail.com>
2023-11-22 17:57:22 +0100

    done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,67 @@
-function CountryDetails() {}
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
 
-export default CountryDetails;
+const CountryDetails = () => {
+
+    const { countryId } = useParams()
+    const [country, setCountry] = useState()
+
+    useEffect(() => {
+        axios.get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+            .then(countryData => {
+                setCountry(countryData.data)
+            })
+            .catch(err => console.log(err))
+    }, [countryId])
+
+    return (
+        !country
+            ?
+            <h1>Loading...</h1>
+            :
+            <>
+                <div className="container">
+                    <p style={{ fontSize: '24px', fontWeight: 'bold' }}>Country Details</p>
+
+                    <h1>{country.name.common}</h1>
+
+                    <table className="table">
+                        <thead></thead>
+                        <tbody>
+                            <tr>
+                                <td style={{ width: '30%' }}>Capital</td>
+                                <td>{country.capital}</td>
+                            </tr>
+                            <tr>
+                                <td>Area</td>
+                                <td>
+                                    {country.area} km
+                                    <sup>2</sup>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Borders</td>
+                                <td>
+                                    <ul style={{ listStyle: 'none' }}>
+                                        {
+                                            country.borders.map(elm => {
+
+                                                return (
+                                                    <li key={elm} >
+                                                        <Link to={`/${elm}`}>{elm}</Link>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </>
+    )
+}
+
+export default CountryDetails
