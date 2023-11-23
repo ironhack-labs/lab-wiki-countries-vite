9ba921b9f67819aeee17cb5bb342c5cb40cc7e86
Pablo Portillo <paabloportillo@gmail.com>
2023-11-23 07:56:39 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import CountryDetailsPage from "./pages/CountryDetailsPage"
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+      <Routes>
+
+        <Route path='/' element={<HomePage />} />
+        <Route path='/:alpha3Code' element={<CountryDetailsPage />} />
+      </Routes>
+
     </div>
   );
 }
