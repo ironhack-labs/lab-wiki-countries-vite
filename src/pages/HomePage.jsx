dae759916e9e46ddd5e4f06c942db3dc0886efd3
agdoy <145876715+agdoy@users.noreply.github.com>
2023-11-24 09:09:51 +0100

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,47 @@
-function HomePage() {}
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { Link } from "react-router-dom";
+
+const baseURL = "https://ih-countries-api.herokuapp.com/countries";
+
+function HomePage() {
+
+    const [country, setCountry] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(baseURL)
+            .then((response) => setCountry(response.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    console.log(country)
+
+
+
+    return (
+        <>
+            <h1>"WikiCountries: Tu guía del mundo"</h1>
+            {
+                country.map(elm => {
+                    return (
+                        <div key={elm._id}>
+                            <img
+                                src={`https://flagpedia.net/data/flags/icon/72x54/${elm.alpha2Code.toLowerCase()}.png`}
+                                alt={`${elm.name.common} flag`} />
+                            <Link to={`/${elm.alpha3Code}`}>{elm.name.common}</Link>
+                        </div>
+
+                    )
+                })
+            }
+        </>
+
+
+
+    )
+}
+
 
 export default HomePage;
