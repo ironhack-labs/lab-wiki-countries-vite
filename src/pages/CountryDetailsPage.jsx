c93fbdf6dcfdbda9ede82efb9f9057e9cfc7d9e9
Lara Aguerre <aguerre.lara@gmail.com>
2024-02-12 20:40:52 +0100

    lab done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,66 @@
-function CountryDetails() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './../Styling/CountryDetailsPage.css'
+
+function CountryDetails() {
+
+    const [country, setCountry] = useState()
+    const [isLoading, setIsLoading] = useState(true)
+
+    const { countryId } = useParams()
+
+    useEffect(() => {
+        loadCountry()
+    }, [countryId])
+
+    const loadCountry = () => {
+
+        axios
+            .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+            .then(response => {
+                setCountry(response.data)
+                setIsLoading(false)
+            })
+            .catch(err => console.log(err))
+    }
+
+    const flagUrl = isLoading ? '' : `https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`;
+
+    const countryBorders = isLoading ? [] : country.borders.map((border, index) => (
+        <p key={index}>{border}</p>
+    ));
+
+
+    return (
+        <div>
+            <h2>Country Details</h2>
+
+            {
+                isLoading
+                    ?
+                    <h1>Loading...</h1>
+                    :
+                    <article>
+                        <img src={flagUrl} alt={country.name.common} />
+                        <h1>{country.name.official}</h1>
+
+                        <div className="styling countryCap">
+                            <p>Capital</p>
+                            <p>{country.capital}</p>
+                        </div>
+                        <div className="styling countryArea">
+                            <p>Area</p>
+                            <p>{country.area}km</p>
+                        </div>
+                        <div className="styling">
+                            <p>Borders</p>
+                            <p className="countryBorders">{countryBorders}</p>
+                        </div>
+                    </article>
+            }
+        </div>
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,54 @@
-function HomePage() {}
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import './../Styling/HomePage.css'
+
+function HomePage() {
+
+    const [countries, setCountries] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => loadCountries(), [])
+
+    const loadCountries = () => {
+
+        axios
+            .get("https://ih-countries-api.herokuapp.com/countries")
+            .then(response => {
+                setCountries(response.data)
+                setIsLoading(false)
+            })
+            .catch(err => console.log(err))
+    }
+
+
+    return (
+        <div className="HomePage">
+            <h1>WikiCountries: Your Guide to the World</h1>
+
+            {
+
+                isLoading
+                    ?
+                    <h2>Cargando...</h2>
+                    :
+
+                    countries.map(elm => {
+
+                        const flagUrl = `https://flagpedia.net/data/flags/icon/72x54/${elm.alpha2Code.toLowerCase()}.png`
+
+                        return (
+                            <article className="countries" key={elm._id}>
+                                <Link to={`/${elm.alpha3Code}`}>
+                                    <img src={flagUrl} alt="" />
+                                    <h3>{elm.name.official}</h3>
+                                </Link>
+                            </article>
+                        )
+                    })
+            }
+        </div>
+    )
+}
 
 export default HomePage;
