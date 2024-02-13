f83ef897d68bac5b036cf8f1295c722569b819e2
Veronica Vezzoli <veronicavezzoli94@gmail.com>
2024-02-13 15:55:21 +0100

    lab done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,50 @@
-function HomePage() {}
+import axios from "axios"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+    const[countries, setCountries] = useState([])
+    const[isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        loadCountries()
+    }, [])
+
+    const loadCountries = () => {
+        axios
+        .get("https://ih-countries-api.herokuapp.com/countries")
+        .then(response =>{
+            setCountries(response.data)
+            setIsLoading(false)
+        })
+        .catch(err => console.log(err))
+    }
+
+
+    return (
+        <div className = "HomeCountries">
+        <h1>WikiCountries: Your Guide to the World</h1>
+        {
+            isLoading
+            ?
+            <h1>Cargando...</h1>
+            :
+            countries.map(state => {
+                    const flagUrl = `https://flagpedia.net/data/flags/icon/72x54/${state.alpha2Code.toLowerCase()}.png`
+                return(
+                    <article key={state._id}>
+                        <Link to={`/${state.alpha3Code}`}>
+                            <h2>{state.name.official}</h2>
+                            <img src={flagUrl} alt="" />
+                            </Link>
+                    </article>
+                )
+            }
+
+            )
+        }
+        </div>
+    )
+}
 
 export default HomePage;
