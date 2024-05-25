93d8f3864fe14b24fd30b4b929ee6b7f7fdb1bf1
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-05-25 16:47:36 +0100

    done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,74 @@
-function CountryDetails() {}
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+
+
+function CountryDetails() {
+    const [country, setCountry] = useState(null)
+    const { countryId } = useParams() // function to call to get dynamic values from url
+
+    const getCountryDetails = async id => {
+       
+
+        try {
+            const response = await axios.get(`https://ih-countries-api.herokuapp.com/countries/${id}`)
+            //console.log(response.data);
+            setCountry(response.data);
+        } catch (error) {
+            console.log('error fetching country')
+        }
+    }
+
+    // we always use useEffect with an API. It stops the page from making infinite calls
+    useEffect(() => {
+        getCountryDetails(countryId);
+    }, [countryId]) // we put countryId dependency here every time when we have a dynamic value in the url
+
+    return (
+        <div className="container">
+        <p style={{fontSize: "24px", fontWeight: 'bold'}}>Country Details</p>
+        {!country && <p>Loading...</p>}
+        {country && (
+            <>
+            <h1>{country && country.name.common}</h1>
+
+            <table className="table">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td style={{width: '30%'}}>Capital</td>
+              <td>{country.capital}</td>
+            </tr>
+            <tr>
+              <td>Area</td>
+              <td>
+                {country.area} km
+                <sup>2</sup>
+              </td>
+            </tr>
+            <tr>
+              <td>Borders</td>
+              <td>
+                <ul>
+                {country.borders.map(border => {
+                    return (
+                        <li key={border}>
+                            <Link to={`/${border}`}>{border}</Link>
+                        </li>
+                    )
+                })}
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </>
+        )}
+
+      </div>
+
+
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,46 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function HomePage() {
+    const [countries, setCountries] = useState([])
+
+    const getCountries = async () => {
+       
+
+        try {
+            const response = await axios.get('https://ih-countries-api.herokuapp.com/countries')
+            // console.log(response.data);
+            setCountries(response.data);
+        } catch (error) {
+            console.log('error fetching countries')
+        }
+    }
+
+    // we always use useEffect with an API. It stops the page from making infinite calls
+    useEffect(() => {
+        getCountries();
+    }, [])
+
+    return (
+        <div className="container" style={{ maxHeight: '90vh', overflow: 'scroll'}}>
+            <h1 style={{fontSize: '24px'}}>WikiCountries: Your Guide to the World</h1>
+
+            <div className="list-group">
+            {countries.map(country => {
+                return (
+                    <Link key={country._id} className="list-group-item list-group-item-action" to={country.alpha3Code}>
+                <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="" />
+                <p>{country.name.common}</p>
+                </Link>
+                )
+            })};
+
+            </div>
+        </div>
+
+    )
+}
 
 export default HomePage;
