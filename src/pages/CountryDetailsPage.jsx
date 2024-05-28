d5a8c262d311d87f2529bb96244fe33568888458
Shamil <compshamil@gmail.com>
2024-05-28 19:17:13 -0400

    lab done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -5,14 +5,12 @@ import { useParams } from "react-router-dom";
 
 function CountryDetails() {
   const [country, setCountry] = useState("");
-  
+  let { countryId } = useParams();
 
   useEffect(() => {
-    
-    let { countryId } = useParams();
     const getCountryDetails = async () => {
       try {
-        const response = axios.get(
+        const response = await axios.get(
           ` https://ih-countries-api.herokuapp.com/countries/${countryId}`
         );
 
@@ -23,62 +21,50 @@ function CountryDetails() {
       }
     };
 
-    CountryDetails();
-  }, []);
+    getCountryDetails();
+  }, [countryId]);
 
   return (
     <div className="container">
+      {country ? (
+        <>
+          <h1>{country.name.common}</h1>
+
+          <table className="table">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td style={{ width: "30%" }}>Capital</td>
+                <td>{country.capital[0]}</td>
+              </tr>
+              <tr>
+                <td>Area</td>
+                <td>
+                  {country.area} km
+                  <sup>2</sup>
+                </td>
+              </tr>
+              <tr>
+                <td>Borders</td>
+                <td>
+                  <ul>
+                    {country.borders &&
+                      country.borders.map((border) => (
+                        <li>
+                          <Link to={`/${border}`}>{border}</Link>
+                        </li>
+                      ))}
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+
       <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
-
-      <h1>France</h1>
-
-      <table className="table">
-        <thead></thead>
-        <tbody>
-          <tr>
-            <td style={{ width: "30%" }}>Capital</td>
-            <td>Paris</td>
-          </tr>
-          <tr>
-            <td>Area</td>
-            <td>
-              551695 km
-              <sup>2</sup>
-            </td>
-          </tr>
-          <tr>
-            <td>Borders</td>
-            <td>
-              <ul>
-                <li>
-                  <a href="/AND">Andorra</a>
-                </li>
-                <li>
-                  <a href="/BEL">Belgium</a>
-                </li>
-                <li>
-                  <a href="/DEU">Germany</a>
-                </li>
-                <li>
-                  <a href="/ITA">Italy</a>
-                </li>
-                <li>
-                  <a href="/LUX">Luxembourg</a>
-                </li>
-                <li>
-                  <a href="/MCO">Monaco</a>
-                </li>
-                <li>
-                  <a href="/ESP">Spain</a>
-                </li>
-                <li>
-                  <a href="/CHE">Switzerland</a>
-                </li>
-              </ul>
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 }
