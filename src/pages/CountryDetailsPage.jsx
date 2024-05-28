19b26a6e8201498bdd58012497c956b06ded7af8
Shamil <compshamil@gmail.com>
2024-05-28 18:58:44 -0400

    lab in progress

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,86 @@
-function CountryDetails() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+function CountryDetails() {
+  const [country, setCountry] = useState("");
+  
+
+  useEffect(() => {
+    
+    let { countryId } = useParams();
+    const getCountryDetails = async () => {
+      try {
+        const response = axios.get(
+          ` https://ih-countries-api.herokuapp.com/countries/${countryId}`
+        );
+
+        console.log(response);
+        setCountry(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    CountryDetails();
+  }, []);
+
+  return (
+    <div className="container">
+      <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+
+      <h1>France</h1>
+
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td style={{ width: "30%" }}>Capital</td>
+            <td>Paris</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              551695 km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                <li>
+                  <a href="/AND">Andorra</a>
+                </li>
+                <li>
+                  <a href="/BEL">Belgium</a>
+                </li>
+                <li>
+                  <a href="/DEU">Germany</a>
+                </li>
+                <li>
+                  <a href="/ITA">Italy</a>
+                </li>
+                <li>
+                  <a href="/LUX">Luxembourg</a>
+                </li>
+                <li>
+                  <a href="/MCO">Monaco</a>
+                </li>
+                <li>
+                  <a href="/ESP">Spain</a>
+                </li>
+                <li>
+                  <a href="/CHE">Switzerland</a>
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,54 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countries, setCountries] = useState("");
+
+  useEffect(() => {
+    const getCountry = async () => {
+      try {
+        const response = await axios.get(
+          "https://ih-countries-api.herokuapp.com/countries"
+        );
+
+        setCountries(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCountry();
+  }, []);
+
+  return (
+    <div
+      className="container"
+      style={{ maxHeight: "90vh", overflow: "scroll" }}
+    >
+      <h1 style={{ fontSize: "24px" }}>
+        WikiCountries: Your Guide to the World
+      </h1>
+
+      {countries ? (
+        countries.map((country) => (
+          <div className="list-group">
+            <Link
+              className="list-group-item list-group-item-action"
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt="No Image"
+              />
+              {country.name.common}
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
 
 export default HomePage;
