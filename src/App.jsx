94e0ee7a47f8581d2791912af89116c33864eb20
annafricks <annafricks33@gmail.com>
2024-05-29 18:08:39 -0400

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
+
+
 
 function App() {
+  
+
+
+
   return (
     <div className="App">
+      <Navbar />
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/:countryId" element={ <CountryDetailsPage /> } />
+        </Routes>
+
     </div>
   );
 }
