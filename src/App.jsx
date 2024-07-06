9ec477f168ce5b25b8672eec41524e2dc0d65eec
Nil E <nil.erdenn@gmail.com>
2024-07-06 18:56:32 +0200

    iteration 2 is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
+// import { Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";  // <== IMPORT
+
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Routes>
+      <Route path="/" element= {<HomePage/>} />
+      <Route path="/:country" element= {<CountryDetails/>} />
+      </Routes>
     </div>
   );
 }
