f44d423d1a16fbcf7003a6619ba8346450bd7ef6
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-07-17 14:58:12 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:countryCode" element={<CountryDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,51 @@
-function CountryDetails() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default CountryDetails;
+function CountryDetailsPage() {
+  const { countryCode } = useParams();
+  const [country, setCountry] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${countryCode}`)
+      .then((response) => {
+        if (response.status === 200) {
+          setCountry(response.data);
+        } else {
+          console.log("Country not found");
+        }
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setIsLoading(false));
+  }, [countryCode]);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <h2>Capital: {country.capital}</h2>
+        <h2>Area: {country.area} km²</h2>
+        {country.borders.length > 0 && (
+          <>
+            <h2>Borders:</h2>
+            <ul className="borders">
+              {[...country.borders].map((border) => {
+                return (
+                  <Link to={`/${border}`} key={border}>
+                    {border}
+                  </Link>
+                );
+              })}
+            </ul>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+export default CountryDetailsPage;
