89f25fa4e2017b8beac8c953ad47a854c9f706e9
elpezpr <gabrielpena@hotmail.com>
2024-07-26 17:28:51 -0400

    revised CountryDetailsPage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-primary mb-3">
-        <div className="container">
-          <a className="navbar-brand" href="/">WikiCountries</a>
-        </div>
-      </nav>
+      <div className="container">
+        <Link className="navbar-brand" to="/">WikiCountries</Link>
+      </div>
+    </nav>
   );
-};
+}
 
-export default Navbar
+export default Navbar;
