a1cb4800c8102555630e26c1f838f26259235635
Ismail <ia.abousalem@gmail.com>
2024-07-26 22:13:28 -0400

    Finished CountryDetailsPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import CountryDetails from "./pages/CountryDetailsPage";
 import Navbar from "./components/Navbar";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/:countryId" element={<CountryDetails />} />
+        <Route path="/:countryId" element={<CountryDetailsPage />} />
       </Routes>
     </>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    axios.get('https://ih-countries-api.herokuapp.com/countries')
-      .then(response => {
-        console.log(response.data); // Tip: Log the response data to understand its structure
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then((response) => {
+        console.log(response.data); // Log the response data to understand its structure
         setCountries(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching the countries data:', error);
+      .catch((error) => {
+        console.error("Error fetching the countries data:", error);
       });
   }, []);
 
   return (
     <>
-      <div className="container" style={{ maxHeight: "90vh", overflow: "scroll" }}>
-        <h1 style={{ fontSize: "24px" }}>WikiCountries: Your Guide to the World</h1>
+      <div
+        className="container"
+        style={{ maxHeight: "90vh", overflow: "scroll" }}
+      >
+        <h1 style={{ fontSize: "24px" }}>
+          WikiCountries: Your Guide to the World
+        </h1>
         <div className="list-group">
-          {countries.map(country => (
-            <a key={country.alpha3Code} className="list-group-item list-group-item-action" href={`/${country.alpha3Code}`}>
+          {countries.map((country) => (
+            <Link
+              key={country.alpha3Code}
+              className="list-group-item list-group-item-action"
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt={`${country.name.common} flag`}
+                style={{ marginRight: "10px" }}
+              />
               {country.name.common}
-            </a>
+            </Link>
           ))}
         </div>
       </div>
