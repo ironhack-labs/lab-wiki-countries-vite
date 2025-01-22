7073f6bbd5c2cac57ef45a498b25eb422a4c4529
Ricardo García <ricardogarciajarillo@gmail.com>
2025-01-22 21:34:14 +0100

    iterations 1 & 2 done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,22 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+
+function HomePage() {
+  const [countries, setCountries] = useState([])
+
+
+  return (
+    <div>
+      
+      <Navbar />
+
+      <div className="container" >
+        <h1>WikiCountries: Your Guide to the World</h1>
+
+        
+      </div>
+    </div>
+    )
+}
 
 export default HomePage;
